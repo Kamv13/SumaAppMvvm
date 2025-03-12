c716400391da02c1e53b85d350eb412e04625d4e
Kamv13 <ksmv233@gmail.com>
2025-03-11 18:07:40 -0600

    EjercicioCompletado

--- SumaAppMvvm/ViewModels/SumaAppViewModel.cs
+++ SumaAppMvvm/ViewModels/SumaAppViewModel.cs
@@ -4,10 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace SumaAppMvvm.ViewModels
 {
-    internal class SumaAppViewModel : ObservableObject
+    public partial class SumaAppViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private double? numero1;
+        [ObservableProperty]
+        private double? numero2;
+        [ObservableProperty]
+        private double resultado;
+
+        private void Alerta(string Titulo, string Mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Titulo, Mensaje, "Aceptar"));
+        }
+
+        [RelayCommand]
+        private void Calcular()
+        {
+
+            if (Numero1 == default(double) || Numero2 == default(double))
+            {
+                Alerta("Advertencia", "Los campos numericos no pueden ir vacios");
+            }
+            try
+            {
+                Resultado = (double)(Numero1 + Numero2);
+            }
+            catch (Exception ex)
+            {
+                Alerta("Advertencia", "Los campos numericos no pueden ir vacios"); 
+            }
+        }
+        [RelayCommand]
+        private void Limpiar()
+        {
+            Numero1 = 0;
+            Numero2 = 0;
+            Resultado = 0;
+            
+
+        }
     }
+
 }
+
+
+
--- SumaAppMvvm/Views/SumaAppView.xaml.cs
+++ SumaAppMvvm/Views/SumaAppView.xaml.cs
@@ -3,12 +3,14 @@
 using System.Linq.Expressions;
 using SumaAppMvvm.ViewModels;
 namespace SumaAppMvvm.Views;
+using SumaAppMvvm.ViewModels;
 
 public partial class SumaAppView : ContentPage
 { 
    SumaAppViewModel viewModel = new SumaAppViewModel();
 
-public SumaAppView() {
+public SumaAppView() 
+    {
         InitializeComponent();
 
     viewModel = new SumaAppViewModel();
